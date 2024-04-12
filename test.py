d6834b0f49a7b6517653f7af8f8702ec739571f3
kenny <97374837+kankenny@users.noreply.github.com>
2024-04-12 23:34:58 +0000

    hotfix: remove test file

--- test.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-
-input_file = "test_sequence.fasta"
-
-fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
-for fasta in fasta_sequences:
-    name, sequence = fasta.id, str(fasta.seq)
-    print(f'Name: {name}, Sequence: {sequence}\n')
