a41a975c0836a44a553f542230056f25953b44b9
kenny <97374837+kankenny@users.noreply.github.com>
2024-04-12 23:32:09 +0000

    feat: utilize Biopython for parsing input FASTA file

--- lib/preprocess.py
+++ lib/preprocess.py
@@ -1,72 +1,55 @@
 import re
+from Bio import SeqIO
 from collections import defaultdict
-from lib.util import chrom_name, revcomp, update_dataFrame, sort_table
+from lib.util import revcomp, update_dataFrame, sort_table
 
 
 def process_sequences(args):
-    ref_seq_file = open(args.fasta)
+    fasta_sequences = SeqIO.parse(open(args.fasta), 'fasta')
 
-    ref_seq = []
-    line = ref_seq_file.readline()
+    features = defaultdict(list)
 
-    chrom = chrom_name(line)
-    if chrom != "noID":
-        line = ref_seq_file.readline()
-    else:
-        chrom = line.strip()
+    for fasta in fasta_sequences:
+        if not args.quiet:
+            print(f"Processing {fasta.id}\n")
+        _process_sequence(args, fasta, features)
+
+    return features
+
+
+def _process_sequence(args, fasta, features):
     gquad_list = []
-    eof = False
 
     gb = range(args.minG, args.maxG + 1)[::-1]
     gs = range(3, args.loops + 1)[::-1]
-    features = defaultdict(list)
     longest = (args.maxG + args.maxloop) * args.loops + args.maxG
 
-    while True:
-        if not args.quiet:
-            print(f"Processing {chrom}\n")
-        while line.startswith(">") is False:
-            ref_seq.append(line.strip())
-            line = ref_seq_file.readline()
-            if line == "":
-                eof = True
-                break
-        ref_seq = "".join(ref_seq)
-        ref_seq = ref_seq.upper().replace("U", "T")
+    seq = "".join(fasta.seq)
+    seq = seq.upper().replace("U", "T")
 
-        for g in gb:
-            for s in gs:
-                reg = _create_regex(g, s, args.minloop, args.maxloop)
+    for g in gb:
+        for s in gs:
+            regex = _create_regex(g, s, args.minloop, args.maxloop)
 
-                ref_seq = _process_forward_seq(chrom,
-                                               features,
-                                               gquad_list,
-                                               reg,
-                                               ref_seq,
-                                               longest
-                                               )
+            seq = _process_forward_seq(fasta.id,
+                                       features,
+                                       gquad_list,
+                                       regex,
+                                       seq,
+                                       longest
+                                       )
 
-                if args.noreverse is False:
-                    ref_seq = _process_reverse_seq(chrom,
-                                                   features,
-                                                   gquad_list,
-                                                   reg,
-                                                   ref_seq,
-                                                   longest
-                                                   )
+            if args.noreverse is False:
+                seq = _process_reverse_seq(fasta.id,
+                                           features,
+                                           gquad_list,
+                                           regex,
+                                           seq,
+                                           longest
+                                           )
 
-                _write_to_gff(gquad_list, args.gff_output)
-                gquad_list = []
-
-        if eof:
-            break
-        chrom = chrom_name(line)
-        ref_seq = []
-        line = ref_seq_file.readline()
-        if line == "":
-            break
-
-    return features
+            _write_to_gff(gquad_list, args.gff_output)
+            gquad_list = []
 
 
 def _process_forward_seq(chrom, features, gquad_list, reg, ref_seq, longest):
